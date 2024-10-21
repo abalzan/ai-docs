e7b328e42c6620a4a23068ffc9ce18de300e4eb0
Andrei <andreibalzan@gmail.com>
2024-10-21 08:02:21 +0100

    first commit

---
+++ src/main/java/com/andrei/aidocs/AiDocsApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.aidocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrei/aidocs/AiDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.aidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
